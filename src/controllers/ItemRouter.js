587a0b4db4e37eb0f0a392ade6bb4136f3fc993b
Courtney Macgregor <cjoss90@hotmail.com>
2026-03-29 15:19:22 +1000

    feat: add create item route

--- src/controllers/ItemRouter.js
+++ src/controllers/ItemRouter.js
@@ -8,4 +8,16 @@ itemRouter.get('/', async (request, response) => {
   response.json(items);
 });
 
+itemRouter.post('/', async (request, response) => {
+  const { name, category, stockQuantity } = request.body;
+
+  const newItem = await ItemModel.create({
+    name,
+    category,
+    stockQuantity,
+  });
+
+  response.status(201).json(newItem);
+});
+
 module.exports = itemRouter;
